c426038c1d033c4a3b00e572c31a1a97512a22eb
Brandon <ncsbrandon@yahoo.com>
2022-05-03 19:38:21 -0400

    build jar at home

--- app.sh
+++ app.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-/usr/lib/jvm/jdk-18/bin/java --module-path /home/firstuser/Downloads/openjfx-18.0.1_linux-x64_bin-sdk/javafx-sdk-18.0.1/lib/ --add-modules javafx.controls,javafx.fxml -cp out/artifacts/CVITFusion_Client_jar/CVITFusion-Client.jar com.apextalos.cvitfusionclient.HelloApplication
+/usr/lib/jvm/jdk-18/bin/java --module-path /usr/lib/jvm/javafx-sdk-18.0.1/lib/ --add-modules javafx.controls,javafx.fxml -cp out/artifacts/CVITFusion_Client_jar/CVITFusion-Client.jar com.apextalos.cvitfusionclient.HelloApplication
