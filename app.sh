bccc15b45b04c5329a68ba649374277326883c17
Brandon <ncsbrandon@yahoo.com>
2022-05-03 14:38:01 -0400

    Create app.sh

---
+++ app.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+/usr/lib/jvm/jdk-18//bin/java --module-path /home/firstuser/Downloads/openjfx-18.0.1_linux-x64_bin-sdk/javafx-sdk-18.0.1/lib/ --add-modules javafx.controls,javafx.fxml -cp IdeaProjects/CVITFusion-Client/out/artifacts/CVITFusion_Client_jar/CVITFusion-Client.jar com.apextalos.cvitfusionclient.HelloApplication
