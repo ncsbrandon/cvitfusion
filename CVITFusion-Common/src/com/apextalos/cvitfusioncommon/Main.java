506e83e4a2b326809e4cf5b4711254713727a112
Brandon <ncsbrandon@yahoo.com>
2022-05-08 20:17:42 -0400

    git restructure

---
+++ CVITFusion-Common/src/com/apextalos/cvitfusioncommon/Main.java
@@ -0,0 +1,13 @@
+package com.apextalos.cvitfusioncommon;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Main {
+
+    private static final Logger logger = LogManager.getLogger(Main.class.getSimpleName());
+
+    public void test() {
+        logger.info("Commons INFO");
+    }
+}
---
+++ CVITFusion-Common/src/module-info.java
@@ -0,0 +1,6 @@
+module com.apextalos.cvitfusioncommon {
+    requires org.apache.logging.log4j;
+    exports com.apextalos.cvitfusioncommon;
+
+    opens com.apextalos.cvitfusioncommon;
+}
