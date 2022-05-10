b0dbc3fe02a1cdd6eeef8425e6330153b878ec98
Brandon <ncsbrandon@yahoo.com>
2022-05-09 22:02:52 -0400

    more interesting Main

--- CVITFusion-Common/src/com/apextalos/cvitfusioncommon/Main.java
+++ CVITFusion-Common/src/com/apextalos/cvitfusioncommon/Main.java
@@ -7,7 +7,8 @@ public class Main {
 
     private static final Logger logger = LogManager.getLogger(Main.class.getSimpleName());
 
-    public void test() {
+    public int test() {
         logger.info("Commons INFO");
+        return 69;
     }
 }
