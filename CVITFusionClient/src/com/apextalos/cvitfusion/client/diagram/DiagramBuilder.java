a3a1941f9a9bf21b5679a2fd7e0487150397f3b6
Brandon <ncsbrandon@yahoo.com>
2022-05-12 20:49:18 -0400

    drawing a line

--- CVITFusionClient/src/com/apextalos/cvitfusion/client/diagram/DiagramBuilder.java
+++ CVITFusionClient/src/com/apextalos/cvitfusion/client/diagram/DiagramBuilder.java
@@ -6,10 +6,13 @@ import java.util.List;
 import com.apextalos.cvitfusion.client.controllers.BaseController;
 import com.apextalos.cvitfusion.client.controls.DiagramNodeControl;
 
+import javafx.scene.Node;
+import javafx.scene.shape.Line;
+
 public class DiagramBuilder {
 
-	public List<DiagramNodeControl> fromJSON(String json, BaseController listener) {
-		List<DiagramNodeControl> dncs = new ArrayList<>();
+	public List<Node> fromJSON(String json, BaseController listener) {
+		List<Node> dncs = new ArrayList<>();
 		
 		DiagramNodeControl r = new DiagramNodeControl();
         r.setLayoutX(20);
@@ -17,6 +20,10 @@ public class DiagramBuilder {
         r.getController().getModel().setName("Node 1");
         r.getController().addActionListener(listener);
         dncs.add(r);
+        
+        Line l = new Line(200, 200, 300, 300);
+        l.setStrokeWidth(5);
+        dncs.add(l);
         
         return dncs;
 	}
