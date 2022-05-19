a1182a509900ca04b30ad077e51caf26d20a05a9
Brandon <ncsbrandon@yahoo.com>
2022-05-18 21:02:22 -0400

    refactoring the client structure

--- clean.sh
+++ clean.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 rm -rf CVITFusionClient/target/
+rm -rf CVITFusionClient/.settings/
 rm -rf CVITFusionClient/logs/
 rm -rf cvitfusioncommon/target/
+rm -rf cvitfusioncommon/.settings/
+rm -rf cvitfusionengine/target/
+rm -rf cvitfusionengine/.settings/
