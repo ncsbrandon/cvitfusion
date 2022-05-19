9509e92292804318ad0addbad8bbffb997ddf45e
Brandon <ncsbrandon@yahoo.com>
2022-05-19 13:13:23 -0400

    Add paho to the engine and client.  format poms

--- cvitfusionengine/src/main/java/com/apextalos/cvitfusionengine/mqtt/EngineConfigMqttTransceiver.java
+++ cvitfusionengine/src/main/java/com/apextalos/cvitfusionengine/mqtt/EngineConfigMqttTransceiver.java
@@ -1,8 +1,13 @@
 package com.apextalos.cvitfusionengine.mqtt;
 
 import com.apextalos.cvitfusion.common.mqtt.ConfigMqttTransceiver;
+import com.apextalos.cvitfusion.common.settings.ConfigFile;
 
 public class EngineConfigMqttTransceiver extends ConfigMqttTransceiver {
+
+	public EngineConfigMqttTransceiver(ConfigFile cf) {
+		super(cf);
+	}
 
 	@Override
 	protected void incomingMessage(String topic, String payload) {
