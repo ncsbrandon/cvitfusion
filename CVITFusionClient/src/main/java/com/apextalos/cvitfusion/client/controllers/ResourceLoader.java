233d98264b2830bd30735d0269067108a6139af9
Brandon <ncsbrandon@yahoo.com>
2022-06-27 12:02:56 -0400

    Update ResourceLoader.java

--- CVITFusionClient/src/main/java/com/apextalos/cvitfusion/client/controllers/ResourceLoader.java
+++ CVITFusionClient/src/main/java/com/apextalos/cvitfusion/client/controllers/ResourceLoader.java
@@ -2,14 +2,17 @@ package com.apextalos.cvitfusion.client.controllers;
 
 import java.io.InputStream;
 import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.image.Image;
 
-public class ResourceLoader<T> {
+public class ResourceLoader {
 
 	private static final Logger logger = LogManager.getLogger(ResourceLoader.class.getSimpleName());
 
@@ -18,14 +21,21 @@ public class ResourceLoader<T> {
 	public static final String IMAGE_REFRESH = "refresh.png";
 	public static final String IMAGE_ACCEPT = "accept.png";
 	
-	private T res = null;
+	private static final Map<String, Image> nameImageMapCache = new HashMap<>();
+	private static final Map<String, FXMLResource> nameLoaderMapCache = new HashMap<>();
 	
-	public T getResource() {
-		return res;
+	public FXMLResource createLoader(String name, Object controller) {
+		return createLoader(name, controller, false);
 	}
 	
-	public FXMLLoader createLoader(String name, Object controller) {
+	public FXMLResource createLoader(String name, Object controller, boolean allowCache) {
+		// first check the cache
+		if(allowCache && nameLoaderMapCache.containsKey(name)) {
+			return nameLoaderMapCache.get(name);
+		}
+				
 		FXMLLoader loader = null;
+		Node resource = null;
 
 		try {
 			// try loading as the jar
@@ -35,7 +45,7 @@ public class ResourceLoader<T> {
 				loader = new FXMLLoader();
 				if(controller != null)
 					loader.setController(controller);
-				res = loader.load(in);
+				resource = loader.load(in);
 			} else {
 				// try loading as the debugger
 				URL url = getClass().getResource("../../../../../" + name);
@@ -44,37 +54,49 @@ public class ResourceLoader<T> {
 					loader = new FXMLLoader(url);
 					if(controller != null)
 						loader.setController(controller);
-					res = loader.load();
+					resource = loader.load();
 				} else {
 					logger.error("Unable to find the fxml " + name);
 				}
 			}
 		} catch (Exception e) {
-			logger.error("Failure loading fxml: " + e.getMessage());
+			logger.error("Failure loading fxml ["+name+"]: " + e.getMessage());
 		}
 
-		return loader;
+		FXMLResource fr = new FXMLResource(loader, resource);
+		if(allowCache)
+			nameLoaderMapCache.put(name, fr);
+		return fr;
 	}
 	
 	public Image loadImageByFilename(String name) {
+		// first check the cache
+		if(nameImageMapCache.containsKey(name))
+			return nameImageMapCache.get(name);
+		
 		Image image = null;
 		
-		// try loading as the jar
-		InputStream in = getClass().getResourceAsStream("/" + name);
-		logger.info(String.format("getResourceAsStream is null: %b", in==null));
-		if(in != null) {
-			image = new Image(in);
-		} else {	
-			// try loading as the debugger
-			URL url = getClass().getResource("../../../../../" + name);
-			logger.info(String.format("getResource is null: %b", url==null));
-			if(url != null) {
-				image = new Image(url.toExternalForm());
-			} else {
-				logger.error("unable to find the image " + name);
+		try {
+			// try loading as the jar
+			InputStream in = getClass().getResourceAsStream("/" + name);
+			logger.info(String.format("getResourceAsStream is null: %b", in==null));
+			if(in != null) {
+				image = new Image(in);
+			} else {	
+				// try loading as the debugger
+				URL url = getClass().getResource("../../../../../" + name);
+				logger.info(String.format("getResource is null: %b", url==null));
+				if(url != null) {
+					image = new Image(url.toExternalForm());
+				} else {
+					logger.error("unable to find the image " + name);
+				}
 			}
+		} catch (Exception e) {
+			logger.error("Failure loading fxml ["+name+"]: " + e.getMessage());
 		}
 		
+		nameImageMapCache.put(name, image);
 		return image;
 	}
 }
