59ec82b3feccdbb7cf98b05fde7bd866a1b59bdb
Brandon <ncsbrandon@yahoo.com>
2022-07-04 07:32:59 -0400

    Update TopicBuilder.java

--- cvitfusioncommon/src/main/java/com/apextalos/cvitfusion/common/mqtt/topics/TopicBuilder.java
+++ cvitfusioncommon/src/main/java/com/apextalos/cvitfusion/common/mqtt/topics/TopicBuilder.java
@@ -1,19 +1,19 @@
 package com.apextalos.cvitfusion.common.mqtt.topics;
 
 public class TopicBuilder {
-	
+
 	public static final String REQUEST_PROCESS_STATUS_TOPIC = "/apextalos/cvitfusion/request_process_status/";
 	public static final String RESPOND_PROCESS_STATUS_TOPIC = "/apextalos/cvitfusion/respond_process_status/";
 	public static final String STOP_PROCESS_STATUS_TOPIC = "/apextalos/cvitfusion/stop_process_status/";
-	
+
 	public static final String REQUEST_ENGINE_CONFIG_TOPIC = "/apextalos/cvitfusion/request_engine_config/";
 	public static final String RESPOND_ENGINE_CONFIG_TOPIC = "/apextalos/cvitfusion/respond_engine_config/";
-	
+
 	public static final String SAVE_ENGINE_CONFIG_TOPIC = "/apextalos/cvitfusion/save_engine_config/";
 	public static final String RESULT_ENGINE_CONFIG_TOPIC = "/apextalos/cvitfusion/result_engine_config/";
-	
+
 	public static final String ENGINE_STATUS_TOPIC = "/apextalos/cvitfusion/enginestatus/";
-	
+
 	private TopicBuilder() {
 		// prevent instance
 	}
@@ -21,23 +21,23 @@ public class TopicBuilder {
 	public static String requestProcessStatusAny(String engineID) {
 		return String.format("%s%s/#", REQUEST_PROCESS_STATUS_TOPIC, engineID);
 	}
-	
+
 	public static String requestProcessStatus(String engineID, int processID) {
 		return String.format("%s%s/%d", REQUEST_PROCESS_STATUS_TOPIC, engineID, processID);
 	}
-	
+
 	public static String stopProcessStatusAny(String engineID) {
 		return String.format("%s%s/#", STOP_PROCESS_STATUS_TOPIC, engineID);
 	}
-	
+
 	public static String stopProcessStatus(String engineID, int processID) {
 		return String.format("%s%s/%d", STOP_PROCESS_STATUS_TOPIC, engineID, processID);
 	}
-	
+
 	public static String respondProcessStatus(String engineID, int processID) {
 		return String.format("%s%s/%d", RESPOND_PROCESS_STATUS_TOPIC, engineID, processID);
 	}
-	
+
 	public static String requestConfig(String engineID) {
 		return String.format("%s%s", REQUEST_ENGINE_CONFIG_TOPIC, engineID);
 	}
@@ -45,7 +45,7 @@ public class TopicBuilder {
 	public static String respondConfig(String engineID) {
 		return String.format("%s%s", RESPOND_ENGINE_CONFIG_TOPIC, engineID);
 	}
-	
+
 	public static String saveConfig(String engineID) {
 		return String.format("%s%s", SAVE_ENGINE_CONFIG_TOPIC, engineID);
 	}
